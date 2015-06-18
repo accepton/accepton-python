abaff8e657912471ff9f5988f4411b6820e9858b
Michael Herold <michael.j.herold@gmail.com>
2015-06-18 11:31:45 -0500

    Split requirements between install and test

--- setup.py
+++ setup.py
@@ -1,24 +1,55 @@
 #!/usr/bin/env python
 
 import os
+import re
 import sys
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-with open("README.rst") as readme_file:
-    readme = readme_file.read()
+from setuptools import setup, find_packages
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'accepton'))
 from version import VERSION
+
+
+def parse_requirements(path):
+    """Rudimentary parser for the `requirements.txt` file
+
+    We just want to separate regular packages from links to pass them to the
+    `install_requires` and `dependency_links` params of the `setup()` function
+    properly.
+
+    Borrowed from the httpretty library by Gabriel Falcao, which is licensed
+    under the MIT license.
+
+    https://github.com/gabrielfalcao/HTTPretty/blob/10cbee3471/setup.py#L50
+    """
+    try:
+        requirements = map(str.strip, local_file(path).splitlines())
+    except IOError:
+        raise RuntimeError("Couldn't find the %s file" % path)
+
+    links, packages = [], []
+
+    for requirement in requirements:
+        if not requirement:
+            continue
+        if 'http:' in requirement or 'https:' in requirement:
+            links.append(requirement)
+            name, version = re.findall("\#egg=([^\-]+)-(.+$)", requirement)[0]
+            packages.append("{0}=={1}".format(name, version))
+        else:
+            packages.append(requirement)
+
+    return packages, links
+
+local_file = lambda *f: \
+    open(os.path.join(os.path.dirname(__file__), *f)).read()
+
+install_requires, dependency_links = parse_requirements('requirements.txt')
 
 setup(
     name="accepton",
     version=VERSION,
     description="AcceptOn Python library",
-    long_description=readme,
+    long_description=local_file("README.rst"),
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
@@ -37,10 +68,11 @@ setup(
     author_email="developers@accepton.com",
     url="https://accepton.com",
     license="TODO",
-    packages=["accepton"],
+    packages=find_packages(exclude=["*tests*"]),
     include_package_data=True,
     zip_safe=False,
-    install_requires=["requests >= 2.1.0"],
+    install_requires=install_requires,
+    dependency_links=dependency_links,
     test_suite="tests",
-    tests_require=["httpretty >= 0.8.10"]
+    tests_require=parse_requirements("requirements-test.txt")
 )
